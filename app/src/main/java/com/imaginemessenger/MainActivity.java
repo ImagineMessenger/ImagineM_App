f1eb0b590fd0181e5b14a56fcd3c45b8e2d1ff02
Alessio Spallino <alessiospallino@Alessios-MacBook-Pro.local>
2017-11-10 17:42:21 +0200

    fixing apk problem

--- app/src/main/java/com/imaginemessenger/MainActivity.java
+++ app/src/main/java/com/imaginemessenger/MainActivity.java
@@ -4,13 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.imaginemessenger.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mainListView ;
+    private ArrayAdapter<String> listAdapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         String msg_welcome= "Hi " +username+"!";
         message.setText(msg_welcome);
 
+
+
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -35,5 +47,31 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        ArrayList image_details = getListData();
+        final ListView lv1 = (ListView) findViewById(R.id.custom_list);
+        lv1.setAdapter(new CustomListAdapter(this, image_details));
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Object o = lv1.getItemAtPosition(position);
+                NewMessage newsData = (NewMessage) o;
+                Toast.makeText(MainActivity.this, "Selected :" + " " + newsData, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private ArrayList getListData(){
+        ArrayList<NewMessage> results = new ArrayList<NewMessage>();
+        NewMessage newsData = new NewMessage();
+        newsData.setHeadline("Dance of Democracy");
+        newsData.setReporterName("Pankaj Gupta");
+        newsData.setDate("May 26, 2013, 13:35");
+        results.add(newsData);
+
+        // Add some more dummy data for testing
+        return results;
     }
 }
---
+++ app/src/main/java/com/imaginemessenger/NewMessage.java
@@ -0,0 +1,35 @@
+package com.imaginemessenger;
+
+/**
+ * Created by alessiospallino on 10/11/2017.
+ */
+
+public class NewMessage {
+    private String headline;
+    private String reporterName;
+    private String date;
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getReporterName() {
+        return reporterName;
+    }
+
+    public void setReporterName(String reporterName) {
+        this.reporterName = reporterName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
